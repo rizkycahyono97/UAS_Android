65f4034115000af44d3901e1bb194b0bee61ad1f
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-07-03 17:57:17 +0700

    add viewModel

---
+++ kos_android/app/src/main/java/com/example/kos_android/viewmodel/KostViewModel.kt
@@ -0,0 +1,72 @@
+package com.example.kos_android.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kos_android.data.model.FilterKos
+import com.example.kos_android.data.model.Kost
+import com.example.kos_android.repository.KostRepository
+import kotlinx.coroutines.launch
+
+class KostViewModel(
+    private val kostRepository: KostRepository
+): ViewModel() {
+    //mmenyimpan data view model
+    private val _kostList = MutableLiveData<List<Kost>>()
+    //publiv live data
+    val kostList: LiveData<List<Kost>> = _kostList
+
+    //selected kost
+    private val _selectedKost = MutableLiveData<Kost>()
+    val selectedKost: LiveData<Kost> = _selectedKost
+
+    // LiveData untuk menyimpan state loading (menampilkan/menyembunyikan progress bar)
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    // LiveData untuk menyimpan pesan error
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String> = _errorMessage
+
+    /**
+     * Fungsi untuk mengambil semua data kost dengan filter.
+     * Dipanggil oleh UI.
+     */
+    fun fetchAllKosts(filters: FilterKos) {
+        viewModelScope.launch {
+            _isLoading.postValue(true)
+            try {
+                val result = kostRepository.getAllKosts(filters)
+                //jika berhasil update LiveData
+                _kostList.postValue(result)
+            } catch (e: Exception) {
+                _errorMessage.postValue(e.message)
+            } finally {
+                //sembunyikan loading indikator
+                _isLoading.postValue(false)
+            }
+        }
+    }
+
+    /**
+     * Fungsi untuk mengambil detail satu kost berdasarkan ID.
+     * (Strukturnya akan mirip dengan fetchAllKosts)
+     */
+    fun fetchKostById(id: Long) {
+        viewModelScope.launch {
+            _isLoading.postValue(true)
+            try {
+                val result = kostRepository.getKostById(id)
+                //jika berhasil update LiveData
+                _selectedKost.postValue(result)
+            } catch (e: Exception) {
+                _errorMessage.postValue(e.message)
+            } finally {
+                //sembunyikan loading indikator
+                _isLoading.postValue(false)
+            }
+        }
+    }
+
+}
