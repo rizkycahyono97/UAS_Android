df61777bd3cc83c7bf2b80408350d2f5127dec14
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-07-05 14:19:46 +0700

    modified base url for api

--- kos_android/app/src/main/java/com/example/kos_android/data/network/RetrofitClient.kt
+++ kos_android/app/src/main/java/com/example/kos_android/data/network/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:9000/api/v1/"
+    private const val BASE_URL = "http://10.10.17.101:9000/api/v1/"
 
     //logging
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
